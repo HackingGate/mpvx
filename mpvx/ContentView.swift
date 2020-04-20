44f23393ce1011d00fb2982b160bf1af60ae914b
HackingGate <i@hackinggate.com>
2020-04-20 23:44:20 +0900

    Initial Commit

---
+++ mpvx/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  mpvx
+//
+//  Created by HG on 2020/04/20.
+//  Copyright © 2020 HG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
