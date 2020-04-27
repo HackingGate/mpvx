7c47b93a76f364182b52d149a3911ff4acd50563
HackingGate <i@hackinggate.com>
2020-04-27 23:59:49 +0900

    Improve handle open

--- mpvx/AppDelegate.swift
+++ mpvx/AppDelegate.swift
@@ -11,33 +11,55 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        openFile(nil)
+        displayOpenPannel()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-    @IBAction func openFile(_ sender: AnyObject?) {
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if (!flag) {
+            displayOpenPannel()
+        }
+        return true
+    }
+
+    func application(_ application: NSApplication, open urls: [URL]) {
+        handleOpen(urls)
+    }
+
+    func displayOpenPannel() {
         let panel = NSOpenPanel()
         panel.canCreateDirectories = false
         panel.canChooseFiles = true
         panel.canChooseDirectories = true
         panel.allowsMultipleSelection = true
         if panel.runModal() == .OK {
-            for url in panel.urls {
-                NSDocumentController.shared.noteNewRecentDocumentURL(url)
-                let task = Process()
-                task.launchPath = "/usr/local/bin/mpv"
-                task.arguments = [url.absoluteString]
-                let pipe = Pipe()
-                task.standardOutput = pipe
-                task.launch()
-            }
+            handleOpen(panel.urls)
         }
+    }
+
+    func handleOpen(_ urls: [URL]) {
+        for url in urls {
+            NSDocumentController.shared.noteNewRecentDocumentURL(url)
+        }
+        let args = urls.map( { ($0.absoluteString) } )
+        launchMpv(args)
+    }
+
+    func launchMpv(_ args: [String]) {
+        let task = Process()
+        task.launchPath = "/usr/local/bin/mpv"
+        task.arguments = args
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+    }
+
+    @IBAction func handleMenuOpen(_ sender: AnyObject?) {
+        displayOpenPannel()
     }
 }
 
