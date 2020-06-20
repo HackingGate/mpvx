9cbd733c9e9aeaee3ab7e958e33b081feec78f68
HackingGate <i@hackinggate.com>
2020-06-21 08:56:40 +0900

    Change BUILD to date (in UTC)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 CONFIG_FILE='Config.xcconfig'
 VERSION=`git describe --abbrev=0 --tags --match "[0-9]*"`
-BUILD=`git rev-list HEAD --count`
+BUILD=`date -u +%Y%m%d%H%M%S`
 cp ${CONFIG_FILE}.example ${CONFIG_FILE}
 sed -i '' "s/\(VERSION *= *\).*/\1$VERSION/" $CONFIG_FILE
 sed -i '' "s/\(BUILD *= *\).*/\1$BUILD/" $CONFIG_FILE
