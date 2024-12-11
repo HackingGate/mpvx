27455bcde5a05cb461f93f889e315d8996c3e2e4
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-12-12 00:54:30 +0900

    Remove lefthook from release.yml

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ BUILD=`date -u +%Y%m%d%H%M%S`
 cp ${CONFIG_FILE}.example ${CONFIG_FILE}
 sed -i '' "s/\(VERSION *= *\).*/\1$VERSION/" $CONFIG_FILE
 sed -i '' "s/\(BUILD *= *\).*/\1$BUILD/" $CONFIG_FILE
+echo "Version: $VERSION"
+echo "Build: $BUILD"
