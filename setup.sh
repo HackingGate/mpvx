2ba9cdb8a1ee534ce8b4aec90b51f55d0f3e1184
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-12-21 00:06:46 +0900

    Fix setup.sh for new folder structure

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 CONFIG_FILE='Config.xcconfig'
 VERSION=`git describe --abbrev=0 --tags --match "[0-9]*"`
 BUILD=`date -u +%Y%m%d%H%M%S`
-cp ${CONFIG_FILE}.example ${CONFIG_FILE}
+cp ${CONFIG_FILE}.example mpvx/${CONFIG_FILE}
 sed -i '' "s/\(VERSION *= *\).*/\1$VERSION/" $CONFIG_FILE
 sed -i '' "s/\(BUILD *= *\).*/\1$BUILD/" $CONFIG_FILE
 echo "Version: $VERSION"
