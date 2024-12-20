a48a3cd04330438600604782bab983b3f90dad2c
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-12-21 00:12:57 +0900

    Extend MpvError for LocalizedError

--- mpvx/MpvLauncher.swift
+++ mpvx/MpvLauncher.swift
@@ -10,6 +10,36 @@ enum MpvError: Error {
     case mpvAlreadyRunning
 }
 
+extension MpvError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .mpvAlreadyRunning:
+            return NSLocalizedString("MPV is already running.", comment: "Error when MPV is launched more than once.")
+        }
+    }
+
+    var failureReason: String? {
+        switch self {
+        case .mpvAlreadyRunning:
+            return NSLocalizedString("Another instance of MPV is still active.", comment: "Explanation why MPV can't run.")
+        }
+    }
+
+    var recoverySuggestion: String? {
+        switch self {
+        case .mpvAlreadyRunning:
+            return NSLocalizedString("Please close the existing MPV instance and try again.", comment: "Suggested action for user.")
+        }
+    }
+
+    var helpAnchor: String? {
+        switch self {
+        case .mpvAlreadyRunning:
+            return NSLocalizedString("mpvAlreadyRunningHelp", comment: "Help anchor for more information.")
+        }
+    }
+}
+
 actor MpvLauncher {
     private lazy var mpvTask: Process = {
         return Process()
