ba56e9b49ad8017caf407d42f5273747fbe6a334
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-12-21 23:25:45 +0900

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ CONFIG_FILE='Config.xcconfig'
 VERSION=`git describe --abbrev=0 --tags --match "[0-9]*"`
 BUILD=`date -u +%Y%m%d%H%M%S`
 cp ${CONFIG_FILE}.example mpvx/${CONFIG_FILE}
-sed -i '' "s/\(VERSION *= *\).*/\1$VERSION/" $CONFIG_FILE
-sed -i '' "s/\(BUILD *= *\).*/\1$BUILD/" $CONFIG_FILE
+sed -i '' "s/\(VERSION *= *\).*/\1$VERSION/" mpvx/$CONFIG_FILE
+sed -i '' "s/\(BUILD *= *\).*/\1$BUILD/" mpvx/$CONFIG_FILE
 echo "Version: $VERSION"
 echo "Build: $BUILD"
