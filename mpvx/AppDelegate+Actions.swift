d6343519b18e1197e2d44a3eb238d013b41b3db8
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-12-22 15:28:36 +0900

    Log Analytics events

--- mpvx/AppDelegate+Actions.swift
+++ mpvx/AppDelegate+Actions.swift
@@ -1,7 +1,9 @@
 import Cocoa
+import FirebaseAnalytics
 
 extension AppDelegate {
     internal func displayOpenPanel() {
+        Analytics.logEvent("display_open_panel", parameters: nil)
         panel.canCreateDirectories = false
         panel.canChooseFiles = true
         panel.canChooseDirectories = true
@@ -14,14 +16,23 @@ extension AppDelegate {
     }
 
     @IBAction func handleMenuOpen(_ sender: Any) {
+        Analytics.logEvent("menu_open_clicked", parameters: [
+            "sender": String(describing: sender)
+        ])
         displayOpenPanel()
     }
 
     @IBAction func showMpvxRepo(_ sender: Any) {
+        Analytics.logEvent("mpvx_repo_opened", parameters: [
+            "url": helpURL.absoluteString
+        ])
         NSWorkspace.shared.open(helpURL)
     }
 
     @IBAction func showMpvMannual(_ sender: Any) {
+        Analytics.logEvent("mpv_manual_opened", parameters: [
+            "url": mpvMannualURL.absoluteString
+        ])
         NSWorkspace.shared.open(mpvMannualURL)
     }
 }
